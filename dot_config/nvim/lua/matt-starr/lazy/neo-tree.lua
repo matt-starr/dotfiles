c4b5abc6906d60960250a788b5a50bba64924104
Matt Starr <m@ttstarr.com>
2026-08-05 21:07:55 +1000

    Persisting neo-tree width inside a session

--- dot_config/nvim/lua/matt-starr/lazy/neo-tree.lua
+++ dot_config/nvim/lua/matt-starr/lazy/neo-tree.lua
@@ -1,3 +1,17 @@
+-- Remembers the last manually-resized width for the lifetime of the session,
+-- so toggling neo-tree closed/open doesn't snap it back to the configured default.
+local last_width = 60 -- 1.5x the neo-tree default of 40
+
+vim.api.nvim_create_autocmd("WinResized", {
+    callback = function()
+        for _, winid in ipairs(vim.v.event.windows) do
+            if vim.bo[vim.api.nvim_win_get_buf(winid)].filetype == "neo-tree" then
+                last_width = vim.api.nvim_win_get_width(winid)
+            end
+        end
+    end,
+})
+
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -12,7 +26,7 @@ return {
     opts = {
         window = {
             position = "right",
-            width = 60, -- 1.5x the neo-tree default of 40
+            width = function() return last_width end,
             mappings = {
                 ["o"] = function(state)
                 local node = state.tree:get_node()
